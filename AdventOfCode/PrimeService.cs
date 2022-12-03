fab0ac504d28370d41ceaa5648a63b47e9057513
grace <gracedme.github@gmail.com>
2022-12-03 09:11:16 -0800

    ✅ add simple PrimeService and a unit test

---
+++ AdventOfCode/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿// namespace AdventOfCode;
+using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
